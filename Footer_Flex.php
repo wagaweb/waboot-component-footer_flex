9a9b16e5331e27269d76b7aa0785ada211af1e4b
LostCore <lostcore@gmail.com>
2018-04-10 11:12:30 +0200

    Standardized version

--- Footer_Flex.php
+++ Footer_Flex.php
@@ -4,7 +4,7 @@ Component Name: Footer Flex
 Description: Footer with Widget Area display in Flexbox
 Category: Layout
 Tags: Footer, Flexbox
-Version: 1.0
+Version: 1.0.0
 Author: WAGA Team <dev@waga.it>
 Author URI: http://www.waga.it
  */
