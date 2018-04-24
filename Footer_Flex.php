e2f77a87afbe84636b51aa4a567e2762bb78843f
LostCore <lostcore@gmail.com>
2018-04-24 16:05:36 +0200

    v1.0.2

--- Footer_Flex.php
+++ Footer_Flex.php
@@ -4,7 +4,7 @@ Component Name: Footer Flex
 Description: Footer with Widget Area display in Flexbox
 Category: Layout
 Tags: Footer, Flexbox
-Version: 1.0.0
+Version: 1.0.2
 Author: Waboot Team <info@waboot.io>
 Author URI: http://www.waboot.io
  */
