f92a850cfc0e01abfafdaa19c624bd5a294622e4
fabosway <fabio@waga.it>
2018-04-24 15:59:33 +0200

    Fix Footer Flex Component Markup

--- templates/closure-content.php
+++ templates/closure-content.php
@@ -1,20 +1,18 @@
-<div class="<?php echo $closure_width; ?>">
-    <div class="closure-inner">
+<div class="closure__inner <?php echo $closure_width; ?>">
 
-        <div class="closure-text">
-            <?php echo $footer_text; ?>
-        </div>
-
-        <?php if ( has_nav_menu( 'bottom' ) ) {
-            wp_nav_menu( array(
-                    'theme_location' => 'bottom',
-                    'container'      => false,
-                    'menu_class'     => 'closure-nav'
-                )
-            );
-        } ?>
-
-        <?php do_action('waboot/component/footer-flex/after_bottom_navigation')?>
-
+    <div class="closure__text">
+        <?php echo $footer_text; ?>
     </div>
+
+    <?php if ( has_nav_menu( 'bottom' ) ) {
+        wp_nav_menu( array(
+                'theme_location' => 'bottom',
+                'container'      => false,
+                'menu_class'     => 'closure__nav'
+            )
+        );
+    } ?>
+
+    <?php do_action('waboot/component/footer-flex/after_bottom_navigation')?>
+
 </div>
--- templates/footer-wrapper.php
+++ templates/footer-wrapper.php
@@ -1,25 +1,25 @@
 <div class="footer-flex">
 <div class="<?php echo $footer_width ?>">
-    <div class="footer-widgetarea-wrap">
+    <div class="footer__widgetarea">
         <?php do_action('waboot/component/footer-flex/before_widgets')?>
 
         <?php if ( is_active_sidebar( 'footer-flex-1' ) ) : ?>
-            <div class="footer-widgetarea footer-widgetarea-1">
+            <div class="footer-widget footer-widget--1">
                 <?php dynamic_sidebar( 'footer-flex-1' ); ?>
             </div>
         <?php endif; ?>
         <?php if ( is_active_sidebar( 'footer-flex-2' ) ) : ?>
-            <div class="footer-widgetarea footer-widgetarea-2">
+            <div class="footer-widget footer-widget--2">
                 <?php dynamic_sidebar( 'footer-flex-2' ); ?>
             </div>
         <?php endif; ?>
         <?php if ( is_active_sidebar( 'footer-flex-3' ) ) : ?>
-            <div class="footer-widgetarea footer-widgetarea-3">
+            <div class="footer-widget footer-widget--3">
                 <?php dynamic_sidebar( 'footer-flex-3' ); ?>
             </div>
         <?php endif; ?>
         <?php if ( is_active_sidebar( 'footer-flex-4' ) ) : ?>
-            <div class="footer-widgetarea footer-widgetarea-4">
+            <div class="footer-widget footer-widget--4">
                 <?php dynamic_sidebar( 'footer-flex-4' ); ?>
             </div>
         <?php endif; ?>
@@ -28,6 +28,6 @@
     </div>
 </div>
 </div>
-<div data-cookieonly class="closure site-footer " id="colophon" role="contentinfo">
+<div data-cookieonly class="closure site-closure" id="colophon" role="contentinfo">
     <?php echo $closure_content; ?>
 </div><!-- .closure -->
